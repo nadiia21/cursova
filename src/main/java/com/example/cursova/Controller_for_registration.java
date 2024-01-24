6c4a3bb2f66b41bb69c41d329c560771bf090239
nadiia21 <zorivchak.nadiia@student.uzhnu.edu.ua>
2024-01-24 18:18:20 +0200

    Created a registration window

---
+++ src/main/java/com/example/cursova/Controller_for_registration.java
@@ -0,0 +1,42 @@
+
+package com.example.cursova;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller_for_registration {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button Registration_button;
+
+    @FXML
+    private TextField Registration_login;
+
+    @FXML
+    private TextField Registration_name;
+
+    @FXML
+    private PasswordField Registration_password;
+
+    @FXML
+    private TextField Registration_surname;
+
+    @FXML
+    private Button Return_button;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
