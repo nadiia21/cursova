59aca6043706eb2ea80748c5214c473f6df56729
nadiia21 <zorivchak.nadiia@student.uzhnu.edu.ua>
2024-04-29 18:40:17 +0300

    Added admin panel for Daffodils

--- src/main/java/com/example/cursova/Controller_for_homePage.java
+++ src/main/java/com/example/cursova/Controller_for_homePage.java
@@ -46,7 +46,7 @@ public class Controller_for_homePage {
         img_deer.setOnMouseClicked(event -> openWindow(img_deer, "Deer.fxml", "Оленяча ферма"));
         img_waterfall.setOnMouseClicked(event -> openWindow(img_waterfall, "Waterfall.fxml", "Водоспад Шипіт"));
         img_palace.setOnMouseClicked(event -> openWindow(img_palace, "Palace.fxml", "Палац Шенборнів"));
-        img_daffodils.setOnMouseClicked(event -> openScene(img_daffodils, "Daffodils.fxml", "Долина нарцисів"));
+        img_daffodils.setOnMouseClicked(event -> openWindow(img_daffodils, "Daffodils.fxml", "Долина нарцисів"));
         img_ostriches.setOnMouseClicked(event -> openWindow(img_ostriches, "Ostriches.fxml", "Страусина ферма"));
 
         Btn_return.setOnAction(event -> {
@@ -65,20 +65,6 @@ public class Controller_for_homePage {
                 e.printStackTrace();
             }
         });
-    }
-
-    private void openScene(ImageView imageView, String fxmlFile, String title) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
-            Parent root = fxmlLoader.load();
-            Scene scene = new Scene(root);
-            Stage stage = (Stage) imageView.getScene().getWindow();
-            stage.setScene(scene);
-            stage.setTitle(title);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     private void openWindow(ImageView imageView, String fxmlFile, String title) {
@@ -111,6 +97,11 @@ public class Controller_for_homePage {
                 ostrichesController.loadImage(Ostriches.imagePath1, ostrichesController.imageView1);
                 ostrichesController.loadImage(Ostriches.imagePath2, ostrichesController.imageView2);
                 ostrichesController.loadImage(Ostriches.imagePath3, ostrichesController.imageView3);
+            }else if (controller instanceof Daffodils) {
+                Daffodils daffodilsController = (Daffodils) controller;
+                daffodilsController.loadImage(Daffodils.imagePath1, daffodilsController.imageView1);
+                daffodilsController.loadImage(Daffodils.imagePath2, daffodilsController.imageView2);
+                daffodilsController.loadImage(Daffodils.imagePath3, daffodilsController.imageView3);
             }
             Scene scene = imageView.getScene();
             scene.setRoot(root);
